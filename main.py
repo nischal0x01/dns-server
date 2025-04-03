1f387510d8d7733ec0c373b71c24ef500ce47ba8
nischal0x01 <nischal1337@gmail.com>
2025-04-03 12:56:41 +0545

    response

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ ip = "127.0.0.1" # loopback/local ip
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip,port))
 
+def buildresponse(data):
+    TransactionID = data[:2]
+    pirnt(TransactionID)
+
 while 1:
     data, addr = sock.recvfrom(512)
-    print(data)
+    r = buildresponse(data)
+
+    sock.sendto(r, addr)
