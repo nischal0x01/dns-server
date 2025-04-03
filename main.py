cdf9c6b30f04dd99f21e004127a3da23f2e384ee
nischal0x01 <nischal1337@gmail.com>
2025-04-03 13:03:59 +0545

    defining flags

--- main.py
+++ main.py
@@ -5,12 +5,32 @@ ip = "127.0.0.1" # loopback/local ip
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip,port))
 
+def getFlags(flags):
+    byte1= bytes(flags[:1])
+    byte2= bytes(flags[1:2])
+    rflags= ''
+    QR='1'
+
+    OPCODE=""
+    for bit in range(1,5):
+        OPCODE+=str(ord(byte1)&(1<<bit))
+
+
+
+
+
 def buildresponse(data):
+
+#Transaction ID
     TransactionID = data[:2]
-    pirnt(TransactionID)
+    TID=""
+
+    for byte in TransactionID:
+        TID+= hex(byte[2:])
+# flags
+Flags = getflags(data[2:4])
 
 while 1:
     data, addr = sock.recvfrom(512)
     r = buildresponse(data)
-
     sock.sendto(r, addr)
