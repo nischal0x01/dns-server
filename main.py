78c977c59576a965605e180bd7ee0559dccec3a6
nischal0x01 <nischal1337@gmail.com>
2025-04-03 14:00:10 +0545

    question data

--- main.py
+++ main.py
@@ -15,20 +15,55 @@ def getFlags(flags):
     for bit in range(1,5):
         OPCODE+=str(ord(byte1)&(1<<bit))
 
+        AA='1'
+        TC='0'
+        RD='0'
+        RA='0'
+        Z='000'
+        RCODE = '0000'
+        return int(QR+OPCODE+AA+TC+RD,2).to_bytes(1,byteorder='big')+(RA+Z+RCODE).to_bytes(1,byteorder='big')
 
 
 
+
+
+def getQuestionDomain(data):
+    state=0
+    expectedLength=0
+    domainString=''
+    domainParts = []
+    x=0
+
+    for byte in data:
+        if state == 1:
+            domainString+=chr(byte)
+            if x== expectedLength:
+                domainParts.append(domainString)
+                domainString=''
+        
+        else:
+            state=1
+            expectedLength= byte
+    print(domainString)
 
 def buildresponse(data):
 
-#Transaction ID
+    #Transaction ID
     TransactionID = data[:2]
     TID=""
 
     for byte in TransactionID:
         TID+= hex(byte[2:])
-# flags
-Flags = getflags(data[2:4])
+    # flags
+    Flags = getFlags(data[2:4])
+    print(Flags)
+    
+    #Question Count
+    QDCOUNT = b'\x00\x01'
+
+    #Answer Count
+    getQuestionDomain(data[12:])
+
 
 while 1:
     data, addr = sock.recvfrom(512)
